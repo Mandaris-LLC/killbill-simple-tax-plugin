4bd8972e7b0d33f44634aec0c3df0f3de8a00fb5
Alexander Engel <ae94@icloud.com>
2021-09-30 20:05:51 -0700

    logg

--- src/main/java/org/killbill/billing/plugin/simpletax/config/http/SimpleTaxServlet.java
+++ src/main/java/org/killbill/billing/plugin/simpletax/config/http/SimpleTaxServlet.java
@@ -148,6 +148,7 @@ public class SimpleTaxServlet extends PluginServlet {
         this.taxCountryController = taxCountryController;
         this.vatinController = vatinController;
         this.taxCodeController = taxCodeController;
+        log("SimpleTaxServlet Init");
     }
 
     /**
